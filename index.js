16dc52c29ffac32de1b08d89cc434bace6c50517
Angela <nelimaangela2@gmail.com>
2026-08-04 15:20:35 +0100

    Add the javascript commands

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 const search = document.getElementById("search");
-const dropdown = document.querySelector(".dropdown");
-
-search.addEventListener("focus", () => {
-    dropdown.computedStyleMap.display = "block";
-});
-
-document.addEventListener("click", (e) => {
-    if(!e.target.closest(".search-container")){
-        dropdown.computedStyleMap.display = "none";
-    }
-});
+const dropdown = document.getElementById("dropdown");
