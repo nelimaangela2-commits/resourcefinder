d36f666546f1c8ce6c8ebf55ca8c72ad330ec3f5
Angela <nelimaangela2@gmail.com>
2026-08-04 15:33:30 +0100

    Add the Javascript functions for the search button

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const search = document.getElementById("search");
 const dropdown = document.getElementById("dropdown");
+const items = document.querySelectorAll(".item");
+
+//Show dropdown when input is focused
+search.addEventListener("focus", () => {
+    dropdown.style.display = "block";
+});
+
+//Hide dropdown when clicking outside 
+document.addEventListener("click", (event) => {
+    if (!event.target.closest(".search-container")) {
+        dropdown.style.display = "none";
+    }
+});
+
+// Put selected item into search box 
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        search.value = item.textContent;
+        dropdown.style.display = "none";
+    });
+});
+
+//search button
+document.getElementById("searchBtn").addEventListener("click", () => {
+    alert("Searching for: " + search.value);
+});
